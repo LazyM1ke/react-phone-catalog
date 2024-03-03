04cded6ab41d3c0fb5907bfac590b43d6ee7d389
Mikhail Ivanov <mivanov@24hsoft.ru>
2024-03-04 00:46:40 +0300

    тестовые кнопки перехода между страницами

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,11 +1,22 @@
 import React from "react";
 import styled from "styled-components";
 import Catalog from "../../components/Catalog/Catalog";
+import { Button } from "@mui/material";
+import { useNavigate } from "react-router-dom";
 
 function HomePage() {
+  const navigate = useNavigate();
   return (
     <div>
       <StyledTitle>Телефонная книга компании "Россети Янтарь"</StyledTitle>
+      <StyledBlock>
+        <Button variant="contained" onClick={() => navigate("/login")}>
+          Форма авторизации в админку
+        </Button>
+        <Button variant="contained" onClick={() => navigate("/admin")}>
+          Добавить пользователя
+        </Button>
+      </StyledBlock>
       <Catalog />
     </div>
   );
@@ -15,4 +26,10 @@ export default HomePage;
 
 const StyledTitle = styled.h1`
   text-align: center;
+`;
+
+const StyledBlock = styled.div`
+  display: flex;
+  gap: 20px;
+  width: 500px;
 `;
